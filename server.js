6196a66e1af6acd9b13ff3ac1e1a1998e80d6ef3
Veronica Martinez <vmartinez6615@gmail.com>
2021-01-20 13:51:46 -0500

    deploying with heroku

--- server.js
+++ server.js
@@ -48,6 +48,14 @@ function filterByQuery(query, animalsArray) {
   // return the filtered results:
   return filteredResults;
 }
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -55,6 +63,11 @@ app.get("/api/animals", (req, res) => {
   }
   res.json(results);
 });
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
